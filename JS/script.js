83e97eb3bb8ab4e3f568fa86e185689b7165e263
Rupantar Mondal <rupantarkirnahar@gamil.com>
2022-08-15 03:06:02 +0530

    Made the website reponsive

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["Web Designer" , "Web Developer" , "Programmer", "Web Designer"],
+    typeSpeed : 150,
+    BackSpeed :60,
+    loop : true 
+})
+// for about section typing animation
+var typed = new Typed(".typing-1",{
+    strings:["Web Designer" , "Web Developer" , "Programmer", "Web Designer"],
+    typeSpeed : 200,
+    BackSpeed :60,
+    loop : true 
+})
